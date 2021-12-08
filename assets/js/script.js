89fe08f30c358c3c85097a20bec9a8d631754168
I am Billie <dosbor3@gmail.com>
2021-12-08 14:14:29 -0600

    Add items with the click of a button

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+//assign the button element object representation to a variable in the file:
+var buttonEl = document.querySelector("#save-task");
+
+//adding the DOM object reference 
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+//function that upon user click of the button, create a li
+//set the class name for the li, set the textContent, and append the 
+//li to the ul 
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "Ummm... I know there is something I need to do....";
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+//storing the DOM object reference (list item) in a variable
+buttonEl.addEventListener("click", createTaskHandler); // <-- DONT ADD () behind the function name, serves as function call
+    
+
+
+
