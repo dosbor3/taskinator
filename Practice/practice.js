d561569699a652b1c0bf8760e26a8c69bdbca5a7
I am Billie <dosbor3@gmail.com>
2021-12-10 09:15:40 -0600

    updating

---
+++ Practice/practice.js
@@ -0,0 +1,14 @@
+//pull the content from the h1 element using only the element
+var h1Content = document.querySelector("h1").innerHTML;
+console.dir(h1Content);
+window.alert(h1Content);
+
+//creating an h2 element 
+var h2Content = document.createElement("h2");
+//adding an attribute to the h2 element
+h2Content.setAttribute("name","h2");
+h2Content = "This is the dynamically created h2 element created by JS";
+window.alert(h2Content);
+
+document.querySelector("h2").setEl
+window.alert(document.querySelector("h2"));
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,46 @@
-//we added an event to the entire form, and removed ithe click event listener from the submit button
-//Having the click event listener on the submit button limited the features we can use, so we added the 
-//submit event listener instead of the click event listener to the form.  If we were to keep the click event 
-//listener on the form, each time the user clicks anywhere on the form, the createTaskHandler function would be 
-//called, THIS IS NOT WHAT WE WANT!!!  We only want to browser to respond to a click of the submit button or preseeing
-//the ENTER key.  We will use the form-specific event listener called submit (also called the onsubmit).  This listener
-//listens
-var formEl = document.querySelector("#task-form"); 
-var tasksToDoEl = document.querySelector("#tasks-to-do"); 
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-//
-var createTaskHandler = function(event) { 
-  event.preventDefault(); //legacy browser behavior fix... in the past browsers would refresh the entire page 
-                          //every time you submit the form, this is no longer necessary, as it was used to help
-                          //browsers communicate with servers, but JavaScript does most of this nowbut this default 
-                          //is still in use. As a fix, you would call the preventDefault() which stops the page refreshing 
-                          //that occurs by default
-var taskNameInput = document.querySelector("input[name='task-name']").value;
-var taskTypeInput = document.querySelector("select[name='task-type'").value;
-console.log(taskNameInput);
-console.log(taskTypeInput);
+var taskFormHandler = function(event) {
+  event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name'").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+  // check if inputs are empty (validate)
+  if (taskNameInput === "" || taskTypeInput === "") {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+  
+  formEl.reset();
 
-// create list item
-var listItemEl = document.createElement("li");
-listItemEl.className = "task-item";
+  // reset form fields for next task to be entered
+  document.querySelector("input[name='task-name']").value = "";
+  document.querySelector("select[name='task-type']").selectedIndex = 0;
 
-// create div to hold task info and add to list item
-var taskInfoEl = document.createElement("div");
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput
+  };
 
-// give it a class name
-taskInfoEl.className = "task-info";
+  createTaskEl(taskDataObj);
+};
 
-// add HTML content to div
-taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+var createTaskEl = function(taskDataObj) {
+  // create list item
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
 
-listItemEl.appendChild(taskInfoEl);
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+  listItemEl.appendChild(taskInfoEl);
 
-// add entire list item to list
-tasksToDoEl.appendChild(listItemEl);
-}; 
+  console.dir(listItemEl);
 
-  formEl.addEventListener("submit", createTaskHandler);
-    
+  // add list item to list
+  tasksToDoEl.appendChild(listItemEl);
+};
 
-
-
+formEl.addEventListener("submit", taskFormHandler);
