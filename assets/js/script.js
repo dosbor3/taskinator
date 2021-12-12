90b5f5ca7134051ea7c92aafa18af2c217ae2294
I am Billie <dosbor3@gmail.com>
2021-12-11 19:33:55 -0600

    optimization

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,46 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var formEl = document.querySelector("#task-form");
 
 
-var createTaskHandler = function (event) {
+var taskFormHandler = function(event) {
   event.preventDefault();
-  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskNameInput = document.querySelector("input[name='task-name']").value; //<-- search by attribute name
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-  // create list item
+  // check if input values are empty strings
+if (!taskNameInput || !taskTypeInput) {
+  alert("You need to fill out the task form!");
+  return false;
+}
+
+  // package up data as an object
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput
+};
+
+  // send it as an argument to createTaskEl
+  createTaskEl(taskDataObj);
+}
+
+
+var createTaskEl = function(taskDataObj) {
+  //create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
 
-  // create div to hold task info and add to list item
+  //create div to hold task info and add to list item
   var taskInfoEl = document.createElement("div");
   taskInfoEl.className = "task-info";
-
-  // add HTML content to div
-  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
   listItemEl.appendChild(taskInfoEl);
-
-  // add entire list item to list
+  
+  //add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
-};
+  
+  //reset the form
+  formEl.requestFullscreen();
+ }
 
-formEl.addEventListener("submit", createTaskHandler);
+
+formEl.addEventListener("submit", taskFormHandler);
 
